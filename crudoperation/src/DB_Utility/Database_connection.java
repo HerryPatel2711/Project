9190af1b156f60145e015655a1f67dcef4b46e75
HerryPatel2711 <71713331+HerryPatel2711@users.noreply.github.com>
2020-10-01 16:17:06 +0530

    Update Database_connection.java

--- crudoperation/src/DB_Utility/Database_connection.java
+++ crudoperation/src/DB_Utility/Database_connection.java
@@ -1,5 +1,5 @@
 package DB_Utility;
-
+import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,9 +8,9 @@ public class Database_connection
 {
 	public static Connection getConnection() throws ClassNotFoundException, SQLException
 	{
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/emp","root","");//connection for database
-		return conn;
+		Class.forName("com.mysql.jdbc.Driver"); // Driver for connection database
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/emp","root","");//connection url connection
+		return conn; //return connection object
 		
 	}
 
